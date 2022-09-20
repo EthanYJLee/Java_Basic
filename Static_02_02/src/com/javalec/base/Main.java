387b54a0d8c6c83f4d9fb4a7abd348c5adf820cf
EthanLee <lyj72011648@gmail.com>
2022-09-20 17:25:56 +0900

    Sep20

--- Static_02_02/src/com/javalec/base/Main.java
+++ Static_02_02/src/com/javalec/base/Main.java
@@ -1,9 +1,28 @@
 package com.javalec.base;
 
+import java.util.Scanner;
+
+import com.javalec.function.Gift;
+import com.javalec.function.Person;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Person person1 = new Person();
+		Person person2 = new Person();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("1번 친구에게 선물할 갯수를 입력하세요 : ");
+		person1.giveGift(scanner.nextInt());
+		
+		System.out.println("");
+		
+		if (Gift.total > 0) {
+			System.out.print("2번 친구에게 선물할 갯수를 입력하세요 : ");
+			person2.giveGift(scanner.nextInt());
+		}
 
 	}
 
--- Static_02_02/src/com/javalec/function/Person.java
+++ Static_02_02/src/com/javalec/function/Person.java
@@ -1,5 +1,27 @@
 package com.javalec.function;
 
 public class Person {
+	
+	int gift = 0;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void giveGift(int gift) {
+		Gift.total = Gift.total - gift;
+		
+		if (Gift.total > 0) {
+			System.out.println("남은 선물 갯수는 " + Gift.total + "개입니다.");
+
+		} else if (Gift.total == 0) {
+			System.out.println("남은 선물 갯수가 없습니다.");
+
+		} else {
+			System.out.println("남은 선물 갯수가 모자랍니다.");
+		}
+
+	}
+	
 
 }
